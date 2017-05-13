79f9b10219e4fec63f23c74ead742654ae76915f
Herb Torres <herb.torres@byhook.com>
2017-05-13 13:16:50 -0400

    updates

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import init from './app';
 
 init();
 
-TweenMax.to('.personal', 0.75, {
-  delay: 1,
-  bottom: 0,
-  opacity: 1,
-  ease: Power2.easeOut
-});
+TweenMax.staggerTo(
+  '.top-greeting',
+  1,
+  { bottom: 0, opacity: 1, ease: Power2.easeOut },
+  1
+);
